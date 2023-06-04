ebb24c3fcc3024a1d3a687b43d68b6d12b286997
sadneo <haywardeel@gmail.com>
2023-06-03 20:24:30 -0400

    Fixed silent loadview for nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -30,11 +30,11 @@ vim.api.nvim_create_autocmd({"BufEnter"}, {
 })
 
 vim.api.nvim_create_autocmd({"BufWinLeave"}, {
-	pattern = "*.*",
-	command = "mkview!",
+	pattern = "?*",
+	command = "mkview",
 })
 vim.api.nvim_create_autocmd({"BufWinEnter"}, {
-	pattern = "*.*",
-	command = "silent loadview",
+	pattern = "?*",
+	command = "silent! loadview",
 })
 
