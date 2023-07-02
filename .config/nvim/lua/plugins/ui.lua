3f8cebcb0a27a7bdb3d465523785f7f41b1d27e4
sadneo <haywardeel@gmail.com>
2023-07-01 23:18:56 -0400

    nvim start screen new file keybind is i instead of n

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -23,7 +23,7 @@ dashboard.section.header.val = {
 
 dashboard.section.buttons.val = {
 	dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-	dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("i", " " .. " New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
 	dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
