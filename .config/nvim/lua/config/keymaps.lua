e0da23973c38f984d3358566acf01c8de8c070ac
sadneo <haywardeel@gmail.com>
2023-10-21 16:09:19 -0400

    Added nvim keybinds <Leader>y to "+y <Leader>t to toggling Neorg's concealer

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,6 +5,8 @@ end, {expr=true, noremap=true})
 
 vim.keymap.set("n", "<Space>", "<NOP>")
 vim.keymap.set("n", "<Leader>e", ":Telescope find_files<CR>")
+vim.keymap.set("n", "<Leader>t", ":Neorg toggle-concealer<CR>")
+vim.keymap.set({"n", "v"}, "<Leader>y", "\"+y")
 
 vim.keymap.set("n", "<M-h>", "gT")
 vim.keymap.set("n", "<M-l>", "gt")
