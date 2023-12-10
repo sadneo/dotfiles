31ac5b04c977b379c7ab7fab727dd63a1805ca17
sadneo <haywardeel@gmail.com>
2023-12-10 11:54:33 -0500

    Removed lsp completion from nvim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,12 @@
 return {
     {
         "williamboman/mason.nvim",
-        opts = {},
+        priority = 20,
         config = true,
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        priority = 25,
         config = true,
     },
 	"neovim/nvim-lspconfig",
@@ -13,53 +14,24 @@ return {
         "hrsh7th/nvim-cmp",
         priority = 30,
         dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-cmdline",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
         },
         config = function()
             local cmp = require("cmp")
-            cmp.setup.filetype("rust", {
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                }, {
-                    { name = "buffer" }
-                })
-            })
 
             cmp.setup.cmdline(":", {
                 mapping = cmp.mapping.preset.cmdline(),
                 sources = cmp.config.sources({
-                    { name = "path" }
-                }, {
-                    { name = "cmdline" }
+                    { name = "path" },
+                    {
+                        name = "cmdline",
+                        max_item_count = 10,
+                    },
                 })
             })
 
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            print(capabilities)
             require("lspconfig").rust_analyzer.setup({
-                capabilities = capabilities,
                 on_attach = function(client)
                     require("completion").on_attach(client)
                 end,
@@ -85,6 +57,8 @@ return {
                     }
                 }
             })
-        end},
-    }
+
+        end,
+    },
+}
 
