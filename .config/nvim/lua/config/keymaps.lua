d634e9576f15ca851590655c40bd505c61f7b3a0
sadneo <haywardeel@gmail.com>
2024-04-06 23:56:57 -0400

    Added nvim keybind for deleting to clipboard

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,7 @@ end, {expr=true, noremap=true})
 vim.keymap.set("n", "<Space>", "<NOP>")
 vim.keymap.set({"n", "v"}, "<Leader>y", "\"+y")
 vim.keymap.set({"n", "v"}, "<Leader>p", "\"+p")
+vim.keymap.set({"n", "v"}, "<Leader>d", "\"+d")
 
 vim.keymap.set("n", "<M-h>", "gT")
 vim.keymap.set("n", "<M-l>", "gt")
