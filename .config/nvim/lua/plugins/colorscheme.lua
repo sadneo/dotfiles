04030c73e1f376f208cde332277b9b4763861554
sadneo <haywardeel@gmail.com>
2024-11-27 12:19:52 -0500

    nvim: installed some color schemes, edited modus

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,25 @@
-vim.opt.background = "light"
-
 return {
+    { "folke/tokyonight.nvim" },
+    { "rebelot/kanagawa.nvim" },
+    { "rose-pine/neovim" },
     {
         "miikanissi/modus-themes.nvim",
-        opts = { variant = "tinted" },
+        opts = {
+            variant = "tinted",
+            on_highlights = function(highlights, colors)
+                highlights.DiffAdd = {
+                    bg = colors.bg_main,
+                    fg = colors.fg_added,
+                }
+                highlights.DiffChange = {
+                    bg = colors.bg_main,
+                    fg = colors.fg_changed,
+                }
+                highlights.DiffDelete = {
+                    bg = colors.bg_main,
+                    fg = colors.fg_removed,
+                }
+            end,
+        },
     },
 }
