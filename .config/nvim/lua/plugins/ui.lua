331b853027978e9097b7b7ad4bb1b5d903c04f7f
sadneo <haywardeel@gmail.com>
2024-12-13 13:29:44 -0500

    nvim: changed dashboard to use :e instead of :Explore

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -21,7 +21,7 @@ return {
                 dashboard.button("i", "  New file", ":ene <BAR> startinsert <CR>"),
                 dashboard.button("n", "  Notes", ":Neorg workspace notes <CR>"),
                 dashboard.button("v", "  To do", ":Neorg workspace personal <CR>"),
-                dashboard.button("c", "  Config", ":Explore $XDG_CONFIG_HOME/nvim <CR>"),
+                dashboard.button("c", "  Config", ":e $XDG_CONFIG_HOME/nvim <CR>"),
                 dashboard.button("s", "  Restore Session", ":lua require('persistence').load() <CR>"),
                 dashboard.button("q", "  Quit", ":qa <CR>"),
             }
