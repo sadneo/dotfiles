922f6fcb7950fe250ffa3a07f87a151f7c26f54c
sadneo <haywardeel@gmail.com>
2024-12-18 12:26:12 -0500

    nvim: simplified trouble keybinds

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -138,37 +138,16 @@ return {
     {
         "folke/trouble.nvim",
         cmd = "Trouble",
-        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        opts = {
+            auto_close = true,
+            focus = true,
+            indent_guides = false,
+        },
         keys = {
             {
-                "<leader>xx",
+                "<leader>x",
                 "<cmd>Trouble diagnostics toggle<cr>",
                 desc = "Diagnostics (Trouble)",
-            },
-            {
-                "<leader>xX",
-                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-                desc = "Buffer Diagnostics (Trouble)",
-            },
-            {
-                "<leader>cs",
-                "<cmd>Trouble symbols toggle focus=false<cr>",
-                desc = "Symbols (Trouble)",
-            },
-            {
-                "<leader>cl",
-                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-                desc = "LSP Definitions / references / ... (Trouble)",
-            },
-            {
-                "<leader>xL",
-                "<cmd>Trouble loclist toggle<cr>",
-                desc = "Location List (Trouble)",
-            },
-            {
-                "<leader>xQ",
-                "<cmd>Trouble qflist toggle<cr>",
-                desc = "Quickfix List (Trouble)",
             },
         },
     },
