bfbcfbf141e054a79a41331b8a87387965c094c3
sadneo <haywardeel@gmail.com>
2025-08-10 10:47:28 -0400

    feat(fish): added my aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
+alias tree="eza -T --sort type --git-ignore"
+alias ls="eza --sort type"
+alias ll="eza -lg --sort type"
+alias dot="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
 if status is-interactive
   set fish_greeting
 end
