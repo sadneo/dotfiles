df32e6f0b11c56be059a6885a0e49ce6aa914846
sadneo <haywardeel@gmail.com>
2025-08-26 14:25:58 -0400

    feat(nvim): only source dev plugins if they exist

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -1,7 +1,14 @@
+local dir = vim.fn.expand("~/Projects/neovim-live")
+local stat = vim.loop.fs_stat(dir)
+
+if not stat or not stat.type == "directory" then
+    return {}
+end
+
 return {
     {
         "sadneo/neovim-live",
-        dir = "~/Projects/neovim-live",
+        dir = dir,
         opts = {},
     },
 }
