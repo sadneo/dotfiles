46661239ce7f4be8b6d98d8960fa322307a32e4e
sadneo <haywardeel@gmail.com>
2025-09-02 13:48:11 -0400

    feat(nvim): <Leader>Y to clipboard

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,6 +7,7 @@ vim.keymap.set("n", "<Space>", "<NOP>")
 vim.keymap.set({ "n", "v" }, "<Leader>y", "\"+y")
 vim.keymap.set({ "n", "v" }, "<Leader>p", "\"+p")
 vim.keymap.set({ "n", "v" }, "<Leader>d", "\"+d")
+vim.keymap.set({ "n", "v" }, "<Leader>Y", "\"+Y")
 
 vim.keymap.set("n", "<C-c>", ":%y +<CR>")
 vim.keymap.set("n", "<C-S-l>", function()
