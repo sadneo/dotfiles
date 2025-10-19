d6a2aafeb722752c4033fcbdcc72371c133594fa
sadneo <haywardeel@gmail.com>
2025-10-19 15:40:36 -0400

    feat(nvim): treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,9 +4,28 @@ return {
         build = ":TSUpdate",
         event = { "BufReadPost", "BufNewFile" },
         config = function()
+            ---@diagnostic disable-next-line: missing-fields
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { "lua", "rust", "toml" },
-                highlight = { enable = true },
+                sync_install = false,
+                auto_install = false,
+                ensure_installed = {
+                    "c",
+                    "java",
+                    "lua",
+                    "markdown",
+                    "markdown_inline",
+                    "norg",
+                    "rust",
+                    "toml",
+                    "tsx",
+                    "vim",
+                    "vimdoc",
+                    "yaml",
+                },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = true,
+                },
                 indent = { enable = true },
             })
         end,
