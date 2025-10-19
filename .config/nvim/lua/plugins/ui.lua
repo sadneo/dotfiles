db1ccf024207612e3efb8e6d07a26afcd77e43aa
sadneo <haywardeel@gmail.com>
2025-10-19 14:53:05 -0400

    revert(nvim): removed neorg

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,38 +1,5 @@
 return {
     {
-        "goolord/alpha-nvim",
-        event = "VimEnter",
-        config = function()
-            local dashboard = require("alpha.themes.dashboard")
-
-            dashboard.section.header.val = {
-                [[　　　 　　/＾>》, -―‐‐＜＾}]],
-                [[　　　 　./:::/,≠´::::::ヽ.]],
-                [[　　　　/::::〃::::／}::丿ハ]],
-                [[　　　./:::::i{l|／　ﾉ／ }::}]],
-                [[　　 /:::::::瓜イ＞　´＜ ,:ﾉ]],
-                [[　 ./::::::|ﾉﾍ.{､　(_ﾌ_ノﾉイ＿]],
-                [[　 |:::::::|／}｀ｽ /          /]],
-                [[.　|::::::|(_:::つ/ ThinkPad /　neovim!]],
-                [[.￣￣￣￣￣￣￣＼/＿＿＿＿＿/￣￣￣￣￣]],
-            }
-
-            dashboard.section.buttons.val = {
-                dashboard.button("i", "  New file", ":ene <BAR> startinsert <CR>"),
-                dashboard.button("n", "  Notes", ":Neorg workspace notes <CR>"),
-                dashboard.button("v", "  To do", ":Neorg workspace personal <CR>"),
-                dashboard.button("c", "  Config", ":e $HOME/.config/nvim <CR>"),
-                dashboard.button("s", "  Restore Session", ":lua require('persistence').load() <CR>"),
-                dashboard.button("q", "  Quit", ":qa <CR>"),
-            }
-
-            dashboard.section.footer.val =
-            "Perseverance!"
-
-            require("alpha").setup(dashboard.config)
-        end,
-    },
-    {
         "nvim-lualine/lualine.nvim",
         event = "VeryLazy",
         opts = {
@@ -67,57 +34,13 @@ return {
         keys = {
             {
                 "<Leader>e", function()
-                local dirman = require("neorg").modules.get_module("core.dirman")
-                local workspace = dirman.get_current_workspace()
-
-                require("telescope.builtin").find_files({
-                    cwd = tostring(workspace[2]),
-                })
-            end,
+                    require("telescope.builtin").find_files()
+                end,
             },
             {
                 "<Leader>r", function()
-                require("telescope.builtin").live_grep()
-            end,
-            },
-            {
-                "<Leader>n", function()
-                local dirman = require("neorg").modules.get_module("core.dirman")
-                local workspaces = dirman.get_workspace_names()
-
-                local pickers = require("telescope.pickers")
-                local finders = require("telescope.finders")
-                local conf = require("telescope.config").values
-                local actions = require("telescope.actions")
-                local action_state = require("telescope.actions.state")
-
-                local dropdown_opts = {
-                    layout_config = {
-                        height = #workspaces + 4,
-                    },
-                    borderchars = {
-                        prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
-                        results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
-                        preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-                    },
-                }
-
-                local opts = require("telescope.themes").get_dropdown(dropdown_opts)
-
-                pickers.new(opts, {
-                    prompt_title = "Workspaces",
-                    finder = finders.new_table({ results = workspaces }),
-                    sorter = conf.generic_sorter(opts),
-                    attach_mappings = function(prompt_bufnr, _)
-                        actions.select_default:replace(function()
-                            actions.close(prompt_bufnr)
-                            local selection = action_state.get_selected_entry()
-                            vim.cmd("Neorg workspace " .. selection[1]) -- maybe call lua instead
-                        end)
-                        return true
-                    end,
-                }):find()
-            end,
+                    require("telescope.builtin").live_grep()
+                end,
             },
         },
         opts = {
