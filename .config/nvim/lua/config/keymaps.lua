cb8185925ee7d890e8a883cf4ccd086c36558451
sadneo <haywardeel@gmail.com>
2025-11-25 20:03:37 -0500

    feat: add keybind for C-v

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,15 +11,14 @@ vim.keymap.set({ "n", "v" }, "<Leader>d", "\"+d")
 vim.keymap.set({ "n", "v" }, "<Leader>Y", "\"+Y")
 
 vim.keymap.set("n", "<C-c>", ":%y +<CR>")
+vim.keymap.set("n", "<C-v>", function()
+  vim.cmd("%delete _")
+  vim.cmd("put +")
+end, { desc = "Replace buffer with clipboard" })
+
 vim.keymap.set("n", "<C-S-l>", function()
     vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 end)
 vim.keymap.set("n", "gd", function()
     vim.lsp.buf.definition()
 end)
-
--- user commands
-vim.api.nvim_create_user_command("MasonMappings", function()
-    local mappings = require("mason-lspconfig").get_installed_servers()
-    print(vim.inspect(mappings))
-end, {})
