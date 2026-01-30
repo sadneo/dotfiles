eae9aa50e3afe38f9542e9212233acbe62283c92
sadneo <haywardeel@gmail.com>
2026-01-30 09:32:41 -0500

    migrate: update nvim-treesitter to main

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,34 @@
+local filetypes = {
+    "c",
+    "java",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "norg",
+    "rust",
+    "toml",
+    "tsx",
+    "vim",
+    "vimdoc",
+    "yaml",
+    "latex",
+    "python",
+}
+
 return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         event = { "BufReadPost", "BufNewFile" },
         config = function()
-            ---@diagnostic disable-next-line: missing-fields
-            require("nvim-treesitter.configs").setup({
-                sync_install = false,
-                auto_install = false,
-                ensure_installed = {
-                    "c",
-                    "java",
-                    "lua",
-                    "markdown",
-                    "markdown_inline",
-                    "norg",
-                    "rust",
-                    "toml",
-                    "tsx",
-                    "vim",
-                    "vimdoc",
-                    "yaml",
-                },
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = true,
-                },
-                indent = { enable = true },
+            require("nvim-treesitter").install(filetypes)
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = filetypes,
+                callback = function()
+                    vim.treesitter.start()
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
             })
         end,
-    },
+    }
 }
