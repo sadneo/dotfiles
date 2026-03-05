6bf4cbf29938c26bf9bc697ca0b459326ffc376b
sadneo <haywardeel@gmail.com>
2026-03-05 17:15:29 -0500

    fix(nvim): issue with clipboard in nvim, tmux, and ssh

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,6 +6,18 @@ vim.g.netrw_list_hide = "^./$"
 vim.g.netrw_hide = 1
 vim.g.netrw_bufsettings = "number relativenumber"
 
+vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+}
+
 local opt = vim.opt
 opt.confirm = true
 opt.termguicolors = true
